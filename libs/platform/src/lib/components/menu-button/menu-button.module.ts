194f795e0ff78704247b698b3eea2d0e147b9470
Deepak Kumar <deepak.kumar14@sap.com>
2020-02-26 22:41:28 +0530

    menuTrigger added but implementation pending

--- libs/platform/src/lib/components/menu-button/menu-button.module.ts
+++ libs/platform/src/lib/components/menu-button/menu-button.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MenuButtonComponent } from './menu-button.component';
 import { ButtonModule } from '@fundamental-ngx/core';
+import { FdpMenuModule } from './../menu/menu.module';
 
 @NgModule({
     declarations: [MenuButtonComponent],
-    imports: [CommonModule, ButtonModule],
+    imports: [CommonModule, ButtonModule, FdpMenuModule],
     exports: [MenuButtonComponent]
 })
 export class PlatformMenuButtonModule {}
