e21242dbd1562f31beacf22f040bb40bb4f9a8c6
Deepak Kumar <deepak.kumar14@sap.com>
2020-03-05 19:39:23 +0530

    radio group review needed

--- apps/docs/src/app/platform/component-docs/platform-forms/radio-group/platform-radio-group-docs.component.ts
+++ apps/docs/src/app/platform/component-docs/platform-forms/radio-group/platform-radio-group-docs.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import * as listItemsRadioGroupSrc from '!raw-loader!./platform-radio-group-examples/platform-radio-group-list-items-example.component.html';
+import * as listItemsRadioGroupSrcCode from '!raw-loader!./platform-radio-group-examples/platform-radio-group-list-items-examples.component.ts';
 import * as listRadioGroupSrc from '!raw-loader!./platform-radio-group-examples/platform-radio-group-list-example.component.html';
+import * as listRadioGroupSrcCode from '!raw-loader!./platform-radio-group-examples/platform-radio-group-list-examples.component.ts';
 import { ExampleFile } from '../../../../documentation/core-helpers/code-example/example-file';
 
 @Component({
@@ -14,6 +16,12 @@ export class PlatformRadioGroupDocsComponent implements OnInit {
             language: 'html',
             code: listItemsRadioGroupSrc,
             fileName: 'platform-radio-group-list-items-example'
+        },
+        {
+            language: 'typescript',
+            code: listItemsRadioGroupSrcCode,
+            fileName: 'platform-radio-group-list-items-example',
+            component: 'PlatformMenuBasicExampleComponent'
         }
     ];
 
@@ -22,6 +30,12 @@ export class PlatformRadioGroupDocsComponent implements OnInit {
             language: 'html',
             code: listRadioGroupSrc,
             fileName: 'platform-radio-group-list-example'
+        },
+        {
+            language: 'typescript',
+            code: listRadioGroupSrcCode,
+            fileName: 'platform-radio-group-list-example',
+            component: 'PlatformMenuBasicExampleComponent'
         }
     ];
 
